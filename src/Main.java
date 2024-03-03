45cd6dba2c67a8fa723a7430e579352389fc56f6
JakkoT <jakkoturro@gmail.com>
2024-03-03 12:13:14 +0200

    Uus commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helper Jakko was here ;)");
-	System.out.println("Come on merge conflict, come already");
+	System.out.println("Edit once again");
     }
 }
