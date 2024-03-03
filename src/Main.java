b080393d3d49a58501a2f509a232620546425b9f
Kanamun <martinmanniste15@gmail.com>
2024-03-03 12:13:28 +0200

    confliktid

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helper Jakko was here ;)");
-	System.out.println("Heheheheh)");
+        System.out.println("conflict");
+	System.out.println("conflict");
     }
 }
