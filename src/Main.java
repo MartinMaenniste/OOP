45f463369cc9f096015252584e58886fd9a53b3e
JakkoT <jakkoturro@gmail.com>
2024-03-03 12:08:29 +0200

    Conflict

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helper Jakko was here ;)");
-	System.out.println("Heheheheh)");
+	System.out.println("Come on merge conflict, come already");
     }
 }
